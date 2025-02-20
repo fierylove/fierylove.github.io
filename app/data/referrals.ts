14a04c6a5acea4be89416d6873ede8caf3b99496
Marcus Tan Zhi Hao <zhihao_tan1999@hotmail.com>
2025-02-20 17:46:01 +0800

    Update referrals.ts
    Added referral from prof soroor.

--- app/data/referrals.ts
+++ app/data/referrals.ts
@@ -6,5 +6,13 @@ export const referralsData = [
         message:
         "I had the pleasure of teaching Marcus at Nanyang Polytechnic. He consistently demonstrated curiosity and actively engaged in class discussions. His enthusiasm for learning new technologies was evident, and he quickly adapted to various tools and environments. Marcus would be a valuable addition to any team",
         date: "February 18, 2025",
+    },
+    {
+        name: "Soroor Malekmohammadi",
+        profileImage: "./images/referrals/female_avatar.png", // Replace with actual image URL
+        title: "Assistant Professor, DigiPen Institute of Technology Singapore",
+        message:
+        "I had the pleasure of serving as Marcus’s academic supervisor during his Integrated Work-Study Program (IWSP) and Capstone project. Throughout this time, Marcus consistently demonstrated a strong work ethic and a proactive attitude toward overcoming challenges. He effectively applied theoretical concepts to practical execution, contributing meaningfully to the project’s goals. Marcus exhibited adaptability in handling complex tasks and showed a clear eagerness to enhance his skills. His collaborative nature and commitment made him a valuable team member, and I am confident he will bring these qualities to any professional setting.",
+        date: "February 20, 2025",
     }
 ];
